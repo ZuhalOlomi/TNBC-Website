24410f1db545c7f0dee0db6881079da7784b23f6
Zuhal Olomi <73646826+ZuhalOlomi@users.noreply.github.com>
2026-01-31 11:08:12 -0500

    Update and rename app.py to backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import numpy as np
-import processor # Ensure your processor.py is updated to handle single-packet logic
+import processor
 
 app = Flask(__name__)
 CORS(app)
 
-SUBJECT_ID = "Subject_15"
-DATA_DIR = "./dataset"  # Path to folder containing Subject_15
+# this is lowkey kind of cooked.
 
 # --- HISTORICAL DATA ENDPOINT ---
 @app.route('/api/patient-history', methods=['GET'])
@@ -30,7 +29,6 @@ def get_history():
 
     return jsonify(response)
 
-# --- NEW: REAL-TIME DATA ENDPOINT ---
 @app.route('/api/live-data', methods=['POST'])
 def receive_live_data():
     data = request.json  # Expected format: {"imu": [...], "emg": [...]}
@@ -46,8 +44,6 @@ def receive_live_data():
     live_rom = processor.calculate_rom(imu_array)
     live_rms = processor.calculate_rms(emg_array)
 
-    # In a full implementation, you'd save this to a 'LiveTrial' folder 
-    # or Maria's SD card interface logic here.
     return jsonify({
         "status": "success",
         "current_rom": round(float(live_rom), 1),
@@ -55,5 +51,4 @@ def receive_live_data():
     })
 
 if __name__ == '__main__':
-    # Setting host to '0.0.0.0' allows the ESP32 to connect to your PC's IP
     app.run(debug=True, port=5000, host='0.0.0.0')
